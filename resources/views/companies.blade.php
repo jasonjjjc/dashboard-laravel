660c6e5023be8871bbc32f2d983fca9b42368f21
jasonjjjc <57393155+jasonjjjc@users.noreply.github.com>
2023-09-28 14:17:36 +0100

    add header
    Give it an icon as well as a couple of nav links for help and logout.

--- resources/views/companies.blade.php
+++ resources/views/companies.blade.php
@@ -1,25 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends ('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Companies</title>
-    @vite('resources/css/app.css')
-</head>
+@section('header')
 
-<body>
-    <div class="p-6 flex flex-col gap-6">
-        @foreach ($companies as $company)
-            <div class="flex flex-col border border-gray-100 rounded-sm">
-                <h1 class="py-3 px-6 bg-green-400 text-white rounded-tl-sm rounded-tr-sm">
-                    <a href="/companies/{{ $company->slug }}">{{ $company->title }}</a>
-                </h1>
-                <p class="p-6">{{ $company->excerpt }}</p>
-            </div>
-        @endforeach
-
+    <div class="flex justify-between p-2 border-b-2 border-gray-200">
+        <h1 class="rounded-sm p-2 hover:bg-green-500 hover:text-white cursor-pointer">ComDash</h1>
+        <ul class="flex gap-4">
+            <li class="rounded-sm p-2 hover:bg-green-500 hover:text-white cursor-pointer">Help</li>
+            <li class="rounded-sm p-2 hover:bg-green-500 hover:text-white cursor-pointer">Logout</li>
+        </ul>
     </div>
-</body>
 
-</html>
+@endsection
+
+
+@section ('content')
+<div class="p-6 flex flex-col gap-6">
+
+    @foreach ($companies as $company)
+    <div class="flex flex-col border border-gray-100 rounded-sm">
+        <h1 class="flex">
+            <a href="/companies/{{ $company->slug }}" class="w-full py-3 px-6 bg-green-400 text-white rounded-tl-sm rounded-tr-sm">{{ $company->title }}</a>
+        </h1>
+        <p class="p-6">{{ $company->excerpt }}</p>
+    </div>
+    @endforeach
+
+</div>
+
+@endsection

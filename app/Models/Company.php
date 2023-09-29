7725629ffcc002d0dd29a20766607502fee0976b
jasonjjjc <57393155+jasonjjjc@users.noreply.github.com>
2023-09-29 19:06:11 +0100

    update colours and enable mass assignment
    Updated green tone to darker shade and added a fillable array property to the Company model to enable mass assignment of properties to new entries in the database.

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -1,76 +1,13 @@
 <?php
 
-
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Company
+class Company extends Model
 {
-    public $title;
+    use HasFactory;
 
-    public $excerpt;
-
-    public $date;
-
-    public $slug;
-
-    public $body;
-
-
-    public function __construct($title, $excerpt, $date, $slug, $body)
-    {
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->slug = $slug;
-        $this->body = $body;
-    }
-
-
-
-
-    // find all the details about all the companies in the resources/companies directory
-    public static function all()
-    {
-        return cache()->rememberForever('companies.all', function () {
-            return collect(File::files(resource_path("companies")))
-                ->map(fn ($file) => YamlFrontMatter::parseFile($file))
-                ->map(fn ($document) => new Company(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->slug,
-                    $document->body(),
-                ))
-                ->sortByDesc('date');
-        });
-        
-    }
-
-
-
-    // find one company's details in the resources/companies directory
-    public static function find($slug)
-    {
-        return static::all()->firstWhere('slug', $slug);
-    
-    }
-    
-    
-    // find or fail a company's details in the resources/companies directory
-    public static function findOrFail($slug)
-    {
-        $company = static::find($slug);
-
-        if (!$company) {
-            throw new ModelNotFoundException();
-        }
-
-        return $company;
-
-
-    }
+    protected $fillable = ['name', 'email', 'logo', 'website'];
 }
--- resources/views/companies.blade.php
+++ resources/views/companies.blade.php
@@ -1,13 +1,17 @@
 <x-layout>
-    <div class="p-6 flex flex-1 flex-col gap-6">
+    <div class="px-6 flex flex-1 flex-col gap-6">
 
         @foreach ($companies as $company)
-        <div class="flex flex-col border border-gray-100 rounded-sm">
-            <h1 class="flex">
-                <a href="/companies/{{ $company->slug }}" class="w-full py-3 px-6 bg-green-400 text-white rounded-tl-sm rounded-tr-sm">{{ $company->title }}</a>
-            </h1>
-            <p class="p-6">{{ $company->excerpt }}</p>
-        </div>
+        <a href="/companies/{{ $company->id }}">
+            <div class="flex flex-col items-center border border-gray-100 rounded-md h-72 shadow-md">
+                <h1 class="flex w-full justify-center py-3 px-6 bg-green-900 text-white text-xl rounded-tl-md rounded-tr-md">
+                    {{ $company->name }}
+                </h1>
+                <div class="flex items-center h-full">
+                    <img src="{{ $company->logo }}" class="w-32" />
+                </div>
+            </div>
+        </a>
         @endforeach
     </div>
 </x-layout>

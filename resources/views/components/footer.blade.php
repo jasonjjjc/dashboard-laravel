4d14020419b1c823deb7f8e1c7d3c6bac3e59aeb
jasonjjjc <57393155+jasonjjjc@users.noreply.github.com>
2023-10-03 12:57:17 +0100

    deactivate root links root page
    logos and icons that link back to the root page have been de-activated on the root page to prevent re-renders of the page.

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="flex justify-between items-center border-t border-gray-400 p-2">
         <h1 class="flex ">
-            <a href="/" class="rounded-md py-1 px-3 cursor-pointer hover:text-red-400">&copy;&nbsp;LinkedUp</a>
+            <a href="<?php echo $_SERVER['REQUEST_URI'] != '/' ? '/' : 'javascript:void(0)'; ?>" class="rounded-md py-1 px-3 cursor-pointer hover:text-red-400">&copy;&nbsp;LinkedUp</a>
         </h1>
         <ul class="flex flex-1 justify-end gap-3">
             <li class="rounded-md py-1 px-3 cursor-pointer hover:text-red-400">Cookies</li>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,7 +1,7 @@
 <header>
     <div class="flex justify-between items-center border-b border-gray-400 p-2">
         <h1 class="flex ">
-            <a href="/" class="rounded-md py-1 px-3 cursor-pointer hover:text-red-400">LinkedUp</a>
+            <a href="<?php echo $_SERVER['REQUEST_URI'] != '/' ? '/' : 'javascript:void(0)'; ?>" class="rounded-md py-1 px-3 cursor-pointer hover:text-red-400">LinkedUp</a>
         </h1>
         <ul class="flex flex-1 justify-end gap-3">
             <li class="rounded-md py-1 px-3 cursor-pointer hover:text-red-400">Help</li>

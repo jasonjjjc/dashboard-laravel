a034660e6a5e7c2b13b3b402f90c316e6d84ef6c
jasonjjjc <57393155+jasonjjjc@users.noreply.github.com>
2023-10-03 13:14:40 +0100

    dark detail fields on company page
    improved visibility of fields on company page by making background colour darker instead of border.

--- resources/views/company.blade.php
+++ resources/views/company.blade.php
@@ -1,6 +1,5 @@
 <x-layout>
-
-    <div class="flex flex-1 flex-col px-6">
+    <div class="flex flex-1 flex-col px-6 gap-6">
         <article class="flex flex-col rounded-md shadow-md">
             <div class="relative flex flex-col gap-6 pb-12 rounded-md bg-grey">
                 <h1 class="py-3 px-6 text-xl rounded-tl-md rounded-tr-md">{{ $company->name }}</h1>
@@ -9,18 +8,25 @@
                     <p class="text-sm py-1 px-2 rounded-md bg-black group-hover:text-red-400">{{ $company->category->name }}</p>
                 </a>
                 <div class="px-3 flex flex-col gap-6">
-                    <h2 class="font-bold">Email:</h2>
-                    <p class="p-2 border border-black overflow-hidden">{{ $company->email }}</p>
-                    <h2 class="font-bold">Website:</h2>
-                    <p class="p-2 border border-black overflow-hidden">{{ $company->website }}</p>
-                    <h2 class="font-bold">Updated:</h2>
-                    <p class="p-2 border border-black overflow-hidden">{{ $company->updated_at }}</p>
-                    <h2 class="font-bold">Created:</h2>
-                    <p class="p-2 border border-black overflow-hidden">{{ $company->created_at }}</p>
+                    <div class="flex flex-col gap-2">
+                        <h2 class="font-bold">Email:</h2>
+                        <p class="p-2 bg-black overflow-hidden">{{ $company->email }}</p>
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <h2 class="font-bold">Website:</h2>
+                        <p class="p-2 bg-black overflow-hidden">{{ $company->website }}</p>
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <h2 class="font-bold">Updated:</h2>
+                        <p class="p-2 bg-black overflow-hidden">{{ $company->updated_at }}</p>
+                    </div>
+                    <div class="flex flex-col gap-2">
+                        <h2 class="font-bold">Created:</h2>
+                        <p class="p-2 bg-black overflow-hidden">{{ $company->created_at }}</p>
+                    </div>
                 </div>
             </div>
         </article>
-        <a href="/" class="mt-6 bg-grey py-2 px-6 w-fit rounded-md hover:text-red-400">Home</a>
     </div>
 
 </x-layout>

fa0c9d053828ec8b00a8234c066cc94a70372260
jasonjjjc <57393155+jasonjjjc@users.noreply.github.com>
2023-10-04 17:41:04 +0100

    fix styles issue on featured card
    Redoing the companies page blade refactor because of style bug on featured card on large screens that I was unable to debug.

--- resources/views/companies.blade.php
+++ resources/views/companies.blade.php
@@ -1,17 +1,14 @@
 <x-layout>
     <x-header></x-header>
 
+
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-    @if ($companies->count())    
-    <x-company-featured-card :company="$companies[0]" />
+        <x-company-card-featured :company="$companies[0]" />
 
         <div class="lg:grid lg:grid-cols-2">
             @foreach ($companies->skip(1) as $company)
-            <x-company-card :company="$company" />
+                <x-company-card :company="$company" />
             @endforeach
         </div>
-        @else 
-        <p>No posts yet. Please check back later.</p>
-@endif
     </main>
 </x-layout>
--- resources/views/components/company-card-featured.blade.php
+++ resources/views/components/company-card-featured.blade.php
@@ -1,37 +1,39 @@
+@props(['company'])
+
 <article
     class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
-    <div class="py-6 px-5">
-        <div>
+    <div class="py-6 px-5 lg:flex">
+        <div class="flex-1 lg:mr-8">
             <img src="./images/illustration-1.png" alt="Blog Post illustration" class="rounded-xl">
         </div>
 
-        <div class="mt-8 flex flex-col justify-between">
-            <header>
+        <div class="flex-1 flex flex-col justify-between">
+            <header class="mt-8 lg:mt-0">
                 <div class="space-x-2">
                     <a href="/categories/{{ $company->category->slug }}"
                         class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
                         style="font-size: 10px">{{ $company->category->name }}</a>
-                    <a href="{{ $company->website }}"
-                        class="px-3 py-1 border border-red-300 rounded-full text-red-300 text-xs uppercase font-semibold"
-                        style="font-size: 10px">website</a>
                 </div>
 
                 <div class="mt-4">
-                    <h1 class="text-3xl cursor-pointer">
-                        {{ $company->name }}
+                    <h1 class="text-3xl">
+                        <a href="/companies/{{ $company->slug }}">
+                            {{ $company->name }}
+                        </a>
                     </h1>
-
                     <span class="mt-2 block text-gray-400 text-xs">
                         Published <time>{{ $company->created_at->diffForHumans() }}</time>
                     </span>
                 </div>
             </header>
 
-            <div class="text-sm mt-4">
+            <div class="text-sm mt-2">
                 <p>
-                    <a href="mailto:{{ $company->email }}">
-                        {{ $company->email }}
-                    </a>
+                    {{ $company->email }}
+                </p>
+
+                <p class="mt-4">
+                    {{ $company->website }}
                 </p>
             </div>
 
@@ -39,16 +41,15 @@
                 <div class="flex items-center text-sm">
                     <img src="./images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
-                        <h5 class="font-bold">{{ $company->user->name }}</h5>
+                        <h5 class="font-bold"> {{ $company->user->name }} </h5>
                         <h6>Mascot at Laracasts</h6>
                     </div>
                 </div>
 
-                <div>
+                <div class="hidden lg:block">
                     <a href="/companies/{{ $company->slug }}"
-                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">
-                        Read More
-                    </a>
+                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">Read
+                        More</a>
                 </div>
             </footer>
         </div>

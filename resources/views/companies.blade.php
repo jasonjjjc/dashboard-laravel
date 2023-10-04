a523b87c1e13053bc5c7764d3e3004e347ef8b5c
jasonjjjc <57393155+jasonjjjc@users.noreply.github.com>
2023-10-04 18:53:04 +0100

    Refactor of companies page continues
    Continuing through refactor of companies page.

--- resources/views/companies.blade.php
+++ resources/views/companies.blade.php
@@ -3,12 +3,18 @@
 
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-company-card-featured :company="$companies[0]" />
+        @if ($companies->count())
+            <x-company-card-featured :company="$companies[0]" />
 
-        <div class="lg:grid lg:grid-cols-2">
-            @foreach ($companies->skip(1) as $company)
-                <x-company-card :company="$company" />
-            @endforeach
-        </div>
+            @if ($companies->count() > 1)
+                <div class="lg:grid lg:grid-cols-2">
+                    @foreach ($companies->skip(1) as $company)
+                        <x-company-card :company="$company" />
+                    @endforeach
+                </div>
+            @endif
+        @else
+            <p class="text-center">No companies yet. Please check back later.</p>
+        @endif
     </main>
 </x-layout>
--- resources/views/components/company-card.blade.php
+++ resources/views/components/company-card.blade.php
@@ -1,3 +1,5 @@
+@props(['company'])
+
 <article
     class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
     <div class="py-6 px-5">

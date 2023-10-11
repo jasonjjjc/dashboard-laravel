89900bdba141127f7e20a152fff259888e32b1ce
jasonjjjc <57393155+jasonjjjc@users.noreply.github.com>
2023-10-11 18:43:20 +0100

    clicking company card shows index of employees at that company

--- resources/views/components/company-button.blade.php
+++ resources/views/components/company-button.blade.php
@@ -1,5 +1,5 @@
 @props(['company'])
 
-<a href="/companies/{{ $company->slug }}"
+<a href="/?company={{ $company->slug }}"
     class="px-3 py-1 border border-blue-300 rounded-full text-blue-300 text-xs uppercase font-semibold"
     style="font-size: 10px">{{ $company->name }}</a>
--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,16 @@
 use App\Http\Controllers\EmployeeController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Company;
-use App\Models\Employee;
 use App\Models\User;
 
 
 Route::get('/', [EmployeeController::class, 'index']);
 Route::get('employees/{employee:slug}', [EmployeeController::class, 'show']);
 
-Route::get('users/{user:name}', function (App\Models\User $user) {
-    return view('employees.index', [
-        'employees' => $user->employees,
-        'companies' => Company::all(),
-        'user' => $user,
-    ]);
-});
+// Route::get('users/{user:name}', function (User $user) {
+//     return view('employees.index', [
+//         'employees' => $user->employees,
+//         'companies' => Company::all(),
+//         'user' => $user,
+//     ]);
+// });

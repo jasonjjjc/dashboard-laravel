cec4d4771495ff6030bd1266a393caa5bbecaad4
jasonjjjc <57393155+jasonjjjc@users.noreply.github.com>
2023-10-13 13:45:19 +0100

    users should be able to close the success message
    without being navigated back to the start of the page. And the message should disappear automatically after a few seconds if they do not click to close it.

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,6 +1,6 @@
-<div class="fixed bottom-0 left-0 flex p-4">
+<div x-data="{ isOpen: true }" x-init="setTimeout(() => isOpen = false, 10000)" class="fixed bottom-0 left-0 p-4" x-show="isOpen">
     <div class="py-2 px-4 bg-blue-500 text-white rounded-sm flex gap-8">
         <h1>{{ $message }}</h1>
-        <a href="#" class="h-8 w-12 flex justify-center items-center text-xl font-extrabold hover:text-blue-600 focus:text-blue-600">&times;</a>
+        <a href="#" x-on:click.prevent="isOpen = false" class="h-8 w-12 flex justify-center items-center text-xl font-extrabold hover:text-blue-600 focus:text-blue-600">&times;</a>
     </div>
 </div>

797b689b3d90778402e9bcca7c2b2b860aaab11b
jasonjjjc <57393155+jasonjjjc@users.noreply.github.com>
2023-10-16 18:22:03 +0100

    add form to submit testimonials
    on each employee page

--- resources/views/employees/show.blade.php
+++ resources/views/employees/show.blade.php
@@ -71,7 +71,29 @@
                     <header>
                         <h3 class="font-bold text-xl">Comments</h3>
                     </header>
+
                     <main class="space-y-4">
+
+                        <form method="POST" action="#" class="border border-gray-200 rounded-xl flex flex-col p-6 gap-4">
+                            @csrf
+
+                            <img src="https://i.pravatar.cc/200?id={{ auth()->user()->id }}" alt="avatar"
+                                class="h-12 w-12 rounded-xl">
+
+                            <div class="w-full flex flex-col gap-4 md:space-x-3">
+                                <div class="flex-grow">
+                                    <label for="body" class="sr-only">Enter Your Testimonial Here</label>
+                                    <textarea id="body" name="body" rows="4"
+                                        class="w-full h-full border border-gray-200 rounded-xl placeholder-gray-900 px-4 py-2"
+                                        placeholder="Add a testimonial about {{ $employee->name }}..."></textarea>
+                                </div>
+
+                                <button type="submit"
+                                    class="h-8 bg-blue-500 hover:bg-blue-600 rounded-xl shadow-md text-white px-10 md:w-fit md:self-end">Post</button>
+                            </div>
+                        </form>
+
+
                         @foreach ($employee->testimonials as $testimonial)
                             <x-testimonial :testimonial="$testimonial" />
                         @endforeach

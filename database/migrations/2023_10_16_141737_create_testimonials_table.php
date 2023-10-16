15b29cc5b494876738f2a00b5a2a7dfecc693b63
jasonjjjc <57393155+jasonjjjc@users.noreply.github.com>
2023-10-16 16:21:44 +0100

    refactor foreignId cascade to simplify it

--- database/migrations/2023_10_16_141737_create_testimonials_table.php
+++ database/migrations/2023_10_16_141737_create_testimonials_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
     {
         Schema::create('testimonials', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('employee_id');
-            $table->unsignedBigInteger('user_id');
+            $table->foreignId('employee_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->text('body');
             $table->timestamps();
 
-            $table->foreign('employee_id')->references('id')->on('employees')->onDelete('cascade');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-        });
+            });
     }
 
     /**
